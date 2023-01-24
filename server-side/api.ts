28069059ea20a1ba809028122713a6c50c94d8bc
Oded Answer <oded.t@pepperi.com>
2023-01-24 13:11:36 +0200

    better endpoints

--- server-side/api.ts
+++ server-side/api.ts
@@ -3,71 +3,53 @@ import { FilterObjectService } from './services/filter-object.service';
 import { FilterRuleService } from './services/filter-rule.service';
 import { FilterObject, FilterRule } from './types';
 
-export async function test(client: Client, request: Request) {
-    return {
-        Hello: 'World'
-    }
-};
-
-export async function upsertFilterObject(client: Client, request: Request) {
-    if (!request.body.FilterObject) {
-        throw new Error('Missing FilterObject in request body');
-    }
-
-    const filterObjectService = new FilterObjectService(client);
-    const filterObject: FilterObject = request.body.FilterObject;
-
-    try {
-        const result = await filterObjectService.upsert(filterObject);
-        return result;
-    }
-    catch (ex) {
-        console.error(`Failed to upsert filter object. error - ${ex}`);
-        throw ex;
-    }
-}
-
-export async function getFilterObjects(client: Client, request: Request) {
-    const filterObjectService = new FilterObjectService(client);
-
-    try {
-        const result = await filterObjectService.getAll();
-        return result;
-    }
-    catch (ex) {
-        console.error(`Failed to get filter objects. error - ${ex}`);
-        throw ex;
-    }
-}
-
-export async function upsertFilterRule(client: Client, request: Request) {
-    if (!request.body.FilterRule) {
-        throw new Error('Missing FilterRule in request body');
-    }
-
+export async function filter_rule(client: Client, request: Request) {
     const filterRuleService = new FilterRuleService(client);
-    const filterRule: FilterRule = request.body.FilterRule;
-
     try {
-        const result = await filterRuleService.upsert(filterRule);
-        return result;
+        // if this is GET request, return filter rules
+        if (request.method === 'GET') {
+            const result = await filterRuleService.getAll();
+            return result;
+        }
+
+        // if this is POST request, upsert filter rule
+        else if (request.method === 'POST') {
+            if (!request.body.FilterRule) {
+                throw new Error('Missing FilterRule in request body');
+            }
+            const filterRule: FilterRule = request.body.FilterRule;
+            const result = await filterRuleService.upsert(filterRule);
+            return result;
+        }
     }
     catch (ex) {
-        console.error(`Failed to upsert filter rule. error - ${ex}`);
-        throw ex;
+        console.error(`filter_rule failed. error - ${ex}`);
+        throw new Error((ex as { message: string }).message);
     }
 }
 
-export async function getFilterRules(client: Client, request: Request) {
-    const filterRuleService = new FilterRuleService(client);
-
+export async function filter_object(client: Client, request: Request) {
+    const filterObjectService = new FilterObjectService(client);
     try {
-        const result = await filterRuleService.getAll();
-        return result;
+        // if this is GET request, return filter objects
+        if (request.method === 'GET') {
+            const result = await filterObjectService.getAll();
+            return result;
+        }
+
+        // if this is POST request, upsert filter object
+        else if (request.method === 'POST') {
+            if (!request.body.FilterObject) {
+                throw new Error('Missing FilterObject in request body');
+            }
+            const filterObject: FilterObject = request.body.FilterObject;
+            const result = await filterObjectService.upsert(filterObject);
+            return result;
+        }
     }
     catch (ex) {
-        console.error(`Failed to get filter rules. error - ${ex}`);
-        throw ex;
+        console.error(`filter_object failed. error - ${ex}`);
+        throw new Error((ex as { message: string }).message);
     }
 }
 
